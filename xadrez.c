ea8daeb6be8bc8a4a28b971c8c4b798f0cbae4a5
millerribeiroo <millerrib@gmail.com>
2025-04-14 15:14:35 +0000

    Desafio Intermediário Concluído.

--- xadrez.c
+++ xadrez.c
@@ -7,26 +7,23 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int b = 0;
-    int movbispo;
-    int t = 0;
-    int movtorre;
-    int r = 0;
-    int movrainha;
+    int movbispo, movtorre, movrainha, movcavalo;
+    int i, b, t, r = 0;
     int opcao;
 
+    //Menu inicial do programa:
     printf("Bem vindo ao guia de movimentos de xadrez. \n\n");
- 
     printf("\n1 - Bispo;\n");
     printf("2 - Torre;\n");
-    printf("3 - Rainha;\n\n");
+    printf("3 - Rainha;\n");
+    printf("4 - Cavalo;\n\n");
 
     printf("Escolhe qual peça deseja saber mais sobre: ");
     scanf("%d", &opcao);
 
     switch (opcao)
     {
-    case 1:
+    case 1: //Movimentação do Bispo:
         if (opcao == 1)
         {
             printf("\nOs Bispos movem-se apenas em diagonais e não passam por cima de nenhuma outra peça.");
@@ -46,7 +43,7 @@ int main() {
         
     break;
     
-    case 2:
+    case 2: //Movimentação da Torre:
         if (opcao == 2)
         {
             printf("\nAs Torres movem-se apenas na horizontal ou vertical e não passam por cima de nenhuma outra peça.");
@@ -66,7 +63,7 @@ int main() {
         
     break;
         
-    case 3:
+    case 3: //Movimentação da Rainha:
         if (opcao == 3)
         {
             printf("\nAs Rainhas movem-se nas três direções e não passam por cima de nenhuma outra peça.");
@@ -86,15 +83,38 @@ int main() {
         
     break;       
 
+    
+
+    // Nível Aventureiro - Movimentação do Cavalo
+    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
+    // Um loop pode representar a movimentação horizontal e outro vertical.
+    case 4:
+        if (opcao == 4)
+        {
+            
+            printf("\nOs Cavalos movem-se em 'L' e PODEM passar por cima de outra peça.\n");
+            printf("O movimento consiste em 2 movimentos em uma direção e dobrando mais 1 movimento para o movimento lateral, formando um L.\n");
+            printf("Por exemplo:\n\n");
+
+            for (i = 1; i <= 1; i++)
+                {
+                    for ( movcavalo = 1; movcavalo <= 2; movcavalo++)
+                    {
+                        printf("Cima;");
+                    }
+                    
+                    printf("Direita;");
+                }
+        }  
+    
+    break;
+    
     default:
         printf("Opção inválida.");
     break;
 
     }
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
